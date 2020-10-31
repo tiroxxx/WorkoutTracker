43b48e6b3488d00d2725f691a4d857bcc459d7d0
tiroxxx <christianmarquez41@gmail.com>
2020-10-30 20:59:18 -0400

    deploying to heroku

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-mongoose.connect("mongodb://localhost/workout", {
-    useNewUrlParser: true,
-    useFindAndModify: false
-});
+
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/workout",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
+
 // routes
 app.use(require("./routes/api.js"));
 app.use(require("./routes/view.js"));
